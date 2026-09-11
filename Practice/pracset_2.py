1922dcf7ca5069758335a1897583356f92ea12f6
Shibam <201375636+Wiseman-creator@users.noreply.github.com>
2026-09-12 03:40:10 +0530

    fix practice folder structure

---
+++ Practice/pracset_2.py
@@ -0,0 +1,23 @@
+# Write a python program to add two numbers.
+a = 12
+b = 13
+print(a+b)
+# 2. Write a python program to find remainder when a number is divided by z.
+z=8
+c=(a%z)
+print(c)
+# 3. Check the type of variable assigned using input () function.
+print(type(c))
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not.
+a = 34;b = 80
+if a>=b:
+    print("a is greater than or equal to b ")
+else:
+    print("a is less than or equal to b")
+# 5. Write a python program to find an average of two numbers entered by the user.
+x = float(input("Enter number1:   "))
+y = float(input("Enter number2:   "))
+print("The average is:", (x + y) / 2)
+# 6. Write a python program to calculate the square of a number entered by the user.
+n= int(input("ENter a number:   "))
+print("The square of the number is:",n**2)
