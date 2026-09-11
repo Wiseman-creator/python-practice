e1a6bea87dae07132fc409f6fb648b6514e79044
Shibam <201375636+Wiseman-creator@users.noreply.github.com>
2026-09-12 03:30:38 +0530

    pracset_2

---
+++ Basics/Variables_Datatype.py
@@ -0,0 +1,14 @@
+# variables: is the name given to memory location for a program
+# Data types :
+#  integers( 12,99); floating point(88.5,980.1); stirng("Alice");boolean(Yes,no), None
+# operators : 
+# 1. Arithmetic operators: +, -, *, / etc. 
+# 2. Assignment operators:  =, +=, -= etc. 
+# 3. Comparison operators: ==, >, >=, <,  != etc. 
+# 4. Logical operators: and, or, not. 
+# type function and type casting
+a = 14
+b="12"
+print(type(a))
+print(type(b))
+A = input("Enter name\n")
